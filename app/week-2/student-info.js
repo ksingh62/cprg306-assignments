e2ee3f4a121f18749ed390741006c8f28ead3f0b
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-01-28 23:24:02 -0700

    add student-info component

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Kapilmeet Singh</p>
+      <Link href="https://github.com/ksingh62/cprg306-assignments">
+        Github repo
+      </Link>
+    </div>
+  );
+}
