c0178f659f28e5beb6b3cdd30a5bfeb32be6be34
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-01-28 23:36:02 -0700

    edit link to week-2 page on page.js root

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="http://localhost:3000/week-2"> Week-2 page</Link>
+      <Link href="/week-2"> Week-2 page</Link>
     </>
   );
 }
