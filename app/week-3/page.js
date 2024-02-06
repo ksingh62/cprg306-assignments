5007da7b1b6f32772d2a4ecdbdbf2a41ac36c27a
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-02-06 00:25:36 -0700

    create Page component for week-3

---
+++ app/week-3/page.js
@@ -0,0 +1,12 @@
+import ItemList from "./item-list";
+
+function Page() {
+    return(
+        <main>
+        <h1>Shopping List</h1>
+        <ItemList></ItemList>
+        </main>
+    )
+}
+
+export default Page;
