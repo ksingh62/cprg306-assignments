8925887ed997fc6f839e33b3d8a50ebf1d0c03fa
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-02-06 13:40:51 -0700

    fixed border bug

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,7 +2,7 @@ import ItemList from "./item-list";
 
 function Page() {
   return (
-    <main className="bg-slate-950 text-white container mx-auto p-2">
+    <main className="bg-slate-950 text-white mx-auto p-2">
       <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
       <ItemList></ItemList>
     </main>
