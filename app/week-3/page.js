a5fdf7952f6156aab4fbd81136b3b20ad03bbf75
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-02-06 01:02:10 -0700

    add tailwind classes to week-3 main page

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,12 +1,12 @@
 import ItemList from "./item-list";
 
 function Page() {
-    return(
-        <main>
-        <h1>Shopping List</h1>
-        <ItemList></ItemList>
-        </main>
-    )
+  return (
+    <main className="bg-slate-950 text-white container mx-auto p-2">
+      <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
+      <ItemList></ItemList>
+    </main>
+  );
 }
 
 export default Page;
