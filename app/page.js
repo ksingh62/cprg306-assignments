a376b2e5bd102ffc81f236d09ffd761cb37c97b6
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-02-09 04:42:13 -0700

    add tailwind styles to root page and use map method to render week assignments links

--- app/page.js
+++ app/page.js
@@ -1,13 +1,24 @@
 import Link from "next/link";
 
 export default function Home() {
+  const weeksDone = [2, 3, 4];
+
   return (
-    <>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2"> Week 2 Assignment</Link>
-      <br></br>
-      <Link href="/week-3"> Week 3 Assignment</Link>
-      <Link href="/week-4"> Week 4 Assignment</Link>
-    </>
+    <main className="flex flex-col items-center justify-between p-20 min-h-screen">
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
+        <h1 className="text-4xl font-bold mb-6">
+          CPRG 306: Web Development 2 - Assignments
+        </h1>
+        <div className="text-lg">
+          <ul>
+            {weeksDone.map((week) => (
+              <li key={week} className="hover:text-green-400 hover:underline">
+                <Link href={`/week-${week}`}>Week {week} Assignment</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </main>
   );
 }
