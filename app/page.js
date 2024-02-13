0d7cdf25d7eb05b5f49fe4dbb2646123c19f4e05
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-02-12 21:28:29 -0700

    add link to week-5 page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function Home() {
-  const weeksDone = [2, 3, 4];
+  const weeksDone = [2, 3, 4, 5];
 
   return (
     <main className="flex flex-col items-center justify-between p-20 min-h-screen">
