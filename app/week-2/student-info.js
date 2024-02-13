1fa4490a3e0ff6ebfd72ff03373a2f057215beaa
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-02-13 12:31:55 -0700

    add tailwind classes for styling link element

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,7 @@ export default function StudentInfo() {
   return (
     <div>
       <p>Kapilmeet Singh</p>
-      <Link href="https://github.com/ksingh62/cprg306-assignments">
+      <Link className="hover:text-green-400 hover:underline" href="https://github.com/ksingh62/cprg306-assignments">
         Github repo
       </Link>
     </div>
