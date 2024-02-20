49b437987a7598fd2664e0dfd05e8e7cf2811511
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-02-19 22:42:03 -0700

    modify page.js for week-6

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,10 +1,22 @@
+"use client";
+import { useState } from "react";
 import ItemList from "./item-list";
+import NewItem from "./new-item";
+import itemsData from "./items.json";
 
 function Page() {
+
+  const [items, setItems] = useState(itemsData);
+
+  const handleAddItem = (newItem) => {
+    setItems([...items, newItem]);
+  }
+
   return (
     <main className="bg-slate-950 text-white mx-auto p-2">
       <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
-      <ItemList></ItemList>
+      <NewItem onAddItem={handleAddItem}></NewItem>
+      <ItemList items={items}></ItemList>
     </main>
   );
 }
