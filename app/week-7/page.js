e0bd5b29c433f61e46e00e95f4d2bd56ebf9a65a
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-02-29 23:50:10 -0700

    add mealIdeas component to main page

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import ItemList from "./item-list";
 import NewItem from "./new-item";
 import itemsData from "./items.json";
+import MealIdeas from "./meal-ideas";
 
 function Page() {
   const [items, setItems] = useState(itemsData);
@@ -17,6 +18,7 @@ function Page() {
         <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
         <NewItem onAddItem={handleAddItem}></NewItem>
         <ItemList items={items}></ItemList>
+        <MealIdeas />
       </div>
     </main>
   );
