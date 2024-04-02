ce258d030e9a4e7795e22945c5ae54444fa93e49
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-04-02 02:42:39 -0600

    add link to week-10 page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function Home() {
-  const weeksDone = [2, 3, 4, 5, 6, 7, 8];
+  const weeksDone = [2, 3, 4, 5, 6, 7, 8, 10];
 
   return (
     <main className="flex flex-col items-center justify-between p-20 min-h-screen">
