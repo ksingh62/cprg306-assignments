85d9a9b9c2cbb9c122a79804cd4fed39361466ed
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-04-02 02:41:24 -0600

    delete btn css

--- app/week-10/shopping-list/item.js
+++ app/week-10/shopping-list/item.js
@@ -1,24 +1,25 @@
 function Item({ id, name, quantity, category, onSelect, onDelete }) {
   return (
-    <>
+    <div className="bg-slate-900 mb-4 mt-4 ml-2 container p-2 max-w-sm flex justify-between">
       <div onClick={() => onSelect(name)}>
-        <ul className="bg-slate-900 mb-4 mt-4 ml-2 container p-2 max-w-sm">
+        <ul>
           <li className="text-xl font-bold">{name}</li>
           <li className="text-sm">
             Buy {quantity} in {category}
           </li>
         </ul>
-        <button
+      </div>
+
+      <button
           onClick={(e) => {
             e.stopPropagation();
             onDelete(id);
           }}
-          className="bg-red-500 p-2"
+          className="bg-red-600 p-1 h-8 w-8 font-bold self-center active:bg-red-500"
         >
-          Delete
+          X
         </button>
-      </div>
-    </>
+    </div>
   );
 }
 
